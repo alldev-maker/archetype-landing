f096bb15184554026521ddc2cb6ad2991bd19be8
topdev7 <EugeneNz99@gmail.com>
2022-10-09 12:37:42 -0500

    add login url

--- components/sections/cta.js
+++ components/sections/cta.js
@@ -4,7 +4,11 @@ const CallToAction = () => <section className="section-gap lg:py-[200px] bg-purp
   <div className="container">
     <h2 className="mb-10 text-white font-bold text-4xl lg:text-7xl">Ready to dive in?</h2>
     <div className="flex flex-col md:flex-row md:items-center">
-      <ButtonPrimary addClass='!py-4 !px-10 !bg-white text-purple text-lg lg:text-xl'>Ready to dive in?</ButtonPrimary>
+      <ButtonPrimary addClass='!py-4 !px-10 !bg-white text-purple text-lg lg:text-xl'>
+        <a href='http://app.archetype.dev/'>
+          Ready to dive in?
+        </a>
+      </ButtonPrimary>
       <a href="/" className="mt-4 text-center text-white text-lg md:mt-0 md:ml-9 md:text-left lg:text-xl">Request Sandbox version</a>
     </div>
   </div>
