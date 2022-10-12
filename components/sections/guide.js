fd1d8c2cff5eb56b5129c097b630d1c2f7f04716
topdev7 <EugeneNz99@gmail.com>
2022-10-12 17:52:30 -0500

    feat: update guide style

--- components/sections/guide.js
+++ components/sections/guide.js
@@ -1,28 +1,36 @@
+import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const tabList = [
   {
-    title: 'Setup',
-    content: 'On Archetype, it can take minutes to integrate and immediately start charging customers based on rate limits, quotas, plans, permissions and more. Analytics into your revenue are updated by minute.',
-    image: '/images/phone.png'
+    title: "Dedicated Support",
+    content: "You won’t be left alone on a island. 24/7 support offered for select customers.",
+    image: "/images/phone.png"
   },
   {
-    title: 'Endpoint Specific Pricing',
-    content: 'On Archetype, it can take minutes to integrate and immediately start charging customers based on rate limits, quotas, plans, permissions and more. Analytics into your revenue are updated by minute.',
-    image: '/images/phone.png'
+    title: "Integrations",
+    content: "Playing telephone between data sources slows the process and results in inevitable mistakes. With Archetype, your customer usage and billing logic live in one platform and can be synced to your CRM and accounting software suite.",
+    image: '/images/tool.png'
   },
   {
-    title: 'Custom billable metrics',
-    content: 'On Archetype, it can take minutes to integrate and immediately start charging customers based on rate limits, quotas, plans, permissions and more. Analytics into your revenue are updated by minute.',
-    image: '/images/phone.png'
+    title: "Empower your Sales Team",
+    content: "Playing telephone between data sources slows the process and results in inevitable mistakes. With Archetype, your customer usage and billing logic live in one platform and can be synced to your CRM and accounting software suite.",
+    image: '/images/flash.png'
+  },
+  {
+    title: "15 Minute Integration",
+    content: "Let engineers spend time focusing on your core product and growth. An Archetype integration takes less than 15 minutes and allows you to turn on revenue today. No need to slot this in your roadmap.",
+    image: '/images/explorer.png'
   },
 ]
 
 const Guide = () => {
-  return < section className="section-gap container" >
-    <div className="bg-image">
-      <Tabs>
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return < section className="guide-bg container w-[calc(100%_-_40px)] my-10 bg-[#f1f1f1] lg:bg-opacity-80 shadow-[0_0_40px_rgba(0,0,0,0.25)] rounded-[20px] py-16 px-6 lg:py-[85px] lg:px-[60px] lg:shadow-[0_0_80px_rgba(0,0,0,0.25)] xl:w-full xl:mt-32 xl:mb-48" >
+    <div className="hidden justify-between items-center lg:flex xl:items-start">
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
           {
             tabList.map((item, idx) =>
@@ -33,23 +41,36 @@ const Guide = () => {
         {
           tabList.map((item, idx) =>
             <TabPanel key={idx}>
-              <div className="flex flex-col justify-between lg:flex-row">
-                <div className="w-full mb-10 lg:w-1/2 lg:mb-0">
-                  <h3 className="hidden mb-4 font-semibold text-2xl text-gray lg:block md:mb-6">
-                    {item.title}
-                  </h3>
-                  <p className="text-sm font-light text-gray md:text-base">
-                    {item.content}
-                  </p>
-                </div>
-                <div className="w-full flex justify-center lg:w-1/2">
-                  <img className="w-auto" src={item.image} alt='Guide img' />
-                </div>
-              </div>
+              <h3 className="mb-4 font-semibold text-3xl text-gray">
+                {item.title}
+              </h3>
+              <p className="max-w-[635px] text-xl font-light text-gray">
+                {item.content}
+              </p>
             </TabPanel>
           )
         }
       </Tabs>
+      <div className="flex justify-center items-center shrink-0 bg-[url('/images/frost.png')] bg-no-repeat bg-contain bg-center w-1/3 h-full xl:w-[510px] xl:h-[580px]">
+        <img className="w-auto" src={tabList[tabIndex].image} alt='Guide img' />
+      </div>
+    </div>
+    <div className="lg:hidden">
+      {
+        tabList.map((item, idx) =>
+          <div className="mb-10 last:mb-0" key={idx}>
+            <div className="flex justify-center items-center bg-[url('/images/frost-m.png')] bg-no-repeat bg-contain bg-center m-auto w-[250px] h-[250px] mb-9">
+              <img className="w-auto" src={item.image} alt='Guide img' />
+            </div>
+            <h3 className="mb-4 font-semibold text-lg text-gray text-center">
+              {item.title}
+            </h3>
+            <p className="text-sm font-light text-gray text-center">
+              {item.content}
+            </p>
+          </div>
+        )
+      }
     </div>
   </section>
 }
