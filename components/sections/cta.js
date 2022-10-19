1985d2e3736eb5ec53166b5f66b7e3591f3a1ca5
topdev7 <EugeneNz99@gmail.com>
2022-10-19 04:59:32 -0500

    change button name

--- components/sections/cta.js
+++ components/sections/cta.js
@@ -16,7 +16,7 @@ const CallToAction = () => {
         <h2 className="mb-10 text-white font-bold text-4xl lg:text-7xl">Ready to dive in?</h2>
         <div className="flex flex-col md:flex-row md:items-center">
           <ButtonPrimary addClass='!py-4 !px-10 !bg-white text-purple text-lg lg:text-xl' onPress={handleEvent}>
-            Ready to dive in?
+            Request a demo
           </ButtonPrimary>
           <button className="mt-4 text-center text-white text-lg md:mt-0 md:ml-9 md:text-left lg:text-xl" onClick={handleEvent}>Request Sandbox version</button>
         </div>
